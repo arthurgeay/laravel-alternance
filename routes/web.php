46032ad3d84e09780e67b2ea636bce002faa4bff
Kobweb <yugoplixgamer@gmail.com>
2020-04-29 16:17:08 +0200

    Redirect '/home' route to '/'

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
+Route::get('/home', function (){
+    return redirect('/');
+});
 
 /** COMPANY */
 Route::get('/company', 'CompanyController@index')->name('company.home');
