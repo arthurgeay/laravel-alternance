83814fb6d7de6c5a05dc1b046499ea4a4825f016
Arthur Geay <arthurgeay.contact@gmail.com>
2020-04-29 16:14:58 +0200

    Add route home after register

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 
 /** COMPANY */
 Route::get('/company', 'CompanyController@index')->name('company.home');
