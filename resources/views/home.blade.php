a24e21046986741887765f5b9b5d347c1befc094
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-04-30 17:13:13 +0200

    Adding badges on user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,9 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+        <h1>{{ Auth::user()->name }} <span class="badge"><img src="{{URL::asset('badges/wati_gold.png')}}" alt="" style="position:relative;top:-.5rem;width:4rem" ></span></h1>
             <div class="card">
-                <div class="card-header">Bonjour {{ Auth::user()->name }}</div>
+                <div class="card-header">Bonjour {{ Auth::user()->name }}, vous êtes membres <span class="text-warning">WATIBULLE GOLD</span></div>
 
                 <div class="card-body">
                     @if (session('status'))
