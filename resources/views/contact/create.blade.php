dd56cc90363a41604b598f6e874f89a92099ad70
Valentin <45029982+ValentinMorin@users.noreply.github.com>
2020-05-04 15:38:34 +0200

    Check fields for contact creation

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -7,22 +7,42 @@
             @csrf
             <div class="form-group">
                 <label for="fullname">Nom complet</label>
-                <input type="text" id="fullname" name="fullname" class="form-control" />
+                <input type="text" id="fullname" name="fullname" class="form-control {{ $errors->has('fullname') ? 'is-invalid' : '' }}" value="{{ old('fullname') }}" />
+                @if($errors->has('fullname'))
+                    <div class="invalid-feedback">
+                        Veuillez renseigner un nom de contact
+                    </div>
+                @endif
             </div>
 
             <div class="form-group">
                 <label for="jobname">Poste actuel</label>
-                <input type="text" id="jobname" name="jobname" class="form-control"/>
+                <input type="text" id="jobname" name="jobname" class="form-control {{ $errors->has('jobname') ? 'is-invalid' : '' }}" value="{{ old('jobname') }}" />
+                @if($errors->has('jobname'))
+                    <div class="invalid-feedback">
+                        Veuillez renseigner un poste
+                    </div>
+                @endif
             </div>
 
             <div class="form-group">
                 <label for="mail">Adresse e-mail</label>
-                <input type="text" id="mail" name="mail" class="form-control"/>
+                <input type="text" id="mail" name="mail" class="form-control {{ $errors->has('mail') ? 'is-invalid' : '' }}" value="{{ old('mail') }}" />
+                @if($errors->has('mail'))
+                    <div class="invalid-feedback">
+                        Veuillez renseigner une adresse email
+                    </div>
+                @endif
             </div>
 
             <div class="form-group">
                 <label for="phone">Numéro de téléphone</label>
-                <input type="text" id="phone" name="phone" class="form-control" />
+                <input type="text" id="phone" name="phone" class="form-control {{ $errors->has('phone') ? 'is-invalid' : '' }}" value="{{ old('phone') }}" />
+                @if($errors->has('phone'))
+                    <div class="invalid-feedback">
+                        Veuillez renseigner un numéro de téléphone
+                    </div>
+                @endif
             </div>
 
             <button type="submit" class="btn btn-success">Ajouter</button>
