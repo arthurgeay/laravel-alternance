75f63878381da56a1f7daea34afb80735efd4247
Arthur Geay <arthurgeay.contact@gmail.com>
2020-05-04 16:33:32 +0200

    Responsive for company and application list page

--- resources/views/application/index.blade.php
+++ resources/views/application/index.blade.php
@@ -4,33 +4,35 @@
     <div class="container">
         <h1 class="text-center">Vos demandes</h1>
         <a href="{{ route('application.create') }}" class="btn btn-primary">Ajouter une demande</a>
-        <table class="table">
-            <thead>
-            <tr>
-                <th scope="col">Nom de l'entreprise</th>
-                <th scope="col">Statut</th>
-                <th scope="col">Editer</th>
-                <th scope="col">Supprimer</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($applications as $application)
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
                 <tr>
-                    <th scope="row"><a href="{{ route('company.show', $application->company->id) }}">{{ $application->company->name }}</a></th>
-                    @if($application->state == 'to-do')
-                        <td><span class="badge badge-primary">A faire</span></td>
-                    @elseif($application->state == 'in-progress')
-                        <td><span class="badge badge-info">En cours</span></td>
-                    @elseif($application->state == 'done')
-                        <td><span class="badge badge-success">Terminé</span></td>
-                    @else
-                        <td><span class="badge badge-warning">Rappel</span></td>
-                    @endif
-                    <td><a href="{{ route('application.edit', $application->id) }}">Editer une demande</a></td>
-                    <td><a href="{{ route('application.delete', $application->id) }}">Supprimer une demande</a></td>
+                    <th scope="col">Nom de l'entreprise</th>
+                    <th scope="col">Statut</th>
+                    <th scope="col">Editer</th>
+                    <th scope="col">Supprimer</th>
                 </tr>
-            @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                @foreach($applications as $application)
+                    <tr>
+                        <th scope="row"><a href="{{ route('company.show', $application->company->id) }}">{{ $application->company->name }}</a></th>
+                        @if($application->state == 'to-do')
+                            <td><span class="badge badge-primary">A faire</span></td>
+                        @elseif($application->state == 'in-progress')
+                            <td><span class="badge badge-info">En cours</span></td>
+                        @elseif($application->state == 'done')
+                            <td><span class="badge badge-success">Terminé</span></td>
+                        @else
+                            <td><span class="badge badge-warning">Rappel</span></td>
+                        @endif
+                        <td><a href="{{ route('application.edit', $application->id) }}">Editer une demande</a></td>
+                        <td><a href="{{ route('application.delete', $application->id) }}">Supprimer une demande</a></td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -6,27 +6,29 @@
 
         <a style="position: fixed; top: 10%; left: 1%;" href="{{ route('company.create') }}" class="btn btn-primary">Ajouter une entreprise</a>
 
-        <table class="table">
-            <thead>
-            <tr>
-                <th scope="col">Nom de l'entreprise</th>
-                <th scope="col">Editer</th>
-                <th scope="col">Contacts</th>
-                <th scope="col">Supprimer</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($companies as $company)
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
                 <tr>
-                    <th scope="row"><a href="{{ route('company.show', $company->id) }}">{{ $company->name }}
-                        </a></th>
-                    <td><a href="{{ route('company.edit', $company->id) }}">Editer une entreprise</a></td>
-                    <td><a href="{{ route('contact.create', $company->id) }}">Ajouter un contact</a></td>
-                    <td><a href="{{ route('company.delete', $company->id) }}">Supprimer une entreprise</a></td>
+                    <th scope="col">Nom de l'entreprise</th>
+                    <th scope="col">Editer</th>
+                    <th scope="col">Contacts</th>
+                    <th scope="col">Supprimer</th>
                 </tr>
-            @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                @foreach($companies as $company)
+                    <tr>
+                        <th scope="row"><a href="{{ route('company.show', $company->id) }}">{{ $company->name }}
+                            </a></th>
+                        <td><a href="{{ route('company.edit', $company->id) }}">Editer une entreprise</a></td>
+                        <td><a href="{{ route('contact.create', $company->id) }}">Ajouter un contact</a></td>
+                        <td><a href="{{ route('company.delete', $company->id) }}">Supprimer une entreprise</a></td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 
 
