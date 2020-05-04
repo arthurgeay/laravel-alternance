0c6f4a0ed425372eb3fe73c098da45e99e464701
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-05-04 14:36:21 +0200

    spacing button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,30 +4,32 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <h1>{{ Auth::user()->name }}
-                <span class="badge">
-                @if ($badge < 10)
-                    <img src="{{URL::asset('badges/rsa.png')}}" alt="" style="position:relative;top:-.5rem;width:4rem" >
-                @elseif ($badge >= 10 and $badge < 30)
-                    <img src="{{URL::asset('badges/wati_classic.png')}}" alt="" style="position:relative;top:-.5rem;width:4rem" >
-                @elseif ($badge >= 30 and $badge < 50) 
-                    <img src="{{URL::asset('badges/wati_rose.png')}}" alt="" style="position:relative;top:-.5rem;width:4rem" >
-                @elseif ($badge >= 50 and $badge < 100) 
-                    <img src="{{URL::asset('badges/wati_gold.png')}}" alt="" style="position:relative;top:-.5rem;width:4rem" >
-                @endif
-                </span>
-            </h1>
-            <form method="POST" action="{{ route('home.alcohol') }}">
-                @csrf
-                <input type="hidden" name="userId" value="{{ Auth::user()->id }}">
-                @if (Auth::user()->alcohol)
-                    <input type="hidden" value="0" name="newAlcohol">
-                    <input class="btn btn-info" type="submit" value="Je ne bois pas d'alcool !">
-                @else
-                    <input type="hidden" value="1" name="newAlcohol">
-                    <input class="btn btn-info" type="submit" value="Je suis un gros soifard !">
-                @endif
-            </form>
+            <div class="flex-row d-flex align-items-center justify-content-between">
+                <h1>{{ Auth::user()->name }}
+                    <span class="badge">
+                    @if ($badge < 10)
+                        <img src="{{URL::asset('badges/rsa.png')}}" alt="" style="position:relative;top:-.5rem;width:4rem" >
+                    @elseif ($badge >= 10 and $badge < 30)
+                        <img src="{{URL::asset('badges/wati_classic.png')}}" alt="" style="position:relative;top:-.5rem;width:4rem" >
+                    @elseif ($badge >= 30 and $badge < 50) 
+                        <img src="{{URL::asset('badges/wati_rose.png')}}" alt="" style="position:relative;top:-.5rem;width:4rem" >
+                    @elseif ($badge >= 50 and $badge < 100) 
+                        <img src="{{URL::asset('badges/wati_gold.png')}}" alt="" style="position:relative;top:-.5rem;width:4rem" >
+                    @endif
+                    </span>
+                </h1>
+                <form method="POST" action="{{ route('home.alcohol') }}" class="mb-2">
+                    @csrf
+                    <input type="hidden" name="userId" value="{{ Auth::user()->id }}">
+                    @if (Auth::user()->alcohol)
+                        <input type="hidden" value="0" name="newAlcohol">
+                        <input class="btn btn-info" type="submit" value="Je ne bois pas d'alcool !">
+                    @else
+                        <input type="hidden" value="1" name="newAlcohol">
+                        <input class="btn btn-info" type="submit" value="Je suis un gros soifard !">
+                    @endif
+                </form>
+            </div>
             <div class="card">
                 <div class="card-header">Bonjour {{ Auth::user()->name }}, vous êtes membres                @if ($badge < 10)
                     <span class="text-secondary">chômeurs RSA</span>
