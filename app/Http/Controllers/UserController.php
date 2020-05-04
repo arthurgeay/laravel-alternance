8f3e859ca69c62a9badc64db9d73e08df3dd1975
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-05-04 14:25:00 +0200

    auth controller middleware

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,15 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
+        /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
         $users = User::All();
