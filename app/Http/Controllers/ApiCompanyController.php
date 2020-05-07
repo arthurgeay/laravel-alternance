147120e644e0e6d3aa69a6e93e254e86fe3b96a4
Arthur Geay <arthurgeay.contact@gmail.com>
2020-05-07 14:45:37 +0200

    Add show api feature

--- app/Http/Controllers/ApiCompanyController.php
+++ app/Http/Controllers/ApiCompanyController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Company;
 use Illuminate\Http\Request;
 
 class ApiCompanyController extends Controller
 {
-    //
+    public function show($companyId)
+    {
+        $company = Company::where('id', $companyId)->first();
+        if(!$company) {
+            return response()->json([
+                'statut' => 'Cette entreprise n\'existe pas'
+            ]);
+        }
+
+        return response()->json([
+            'company' => $company
+        ]);
+    }
 }
