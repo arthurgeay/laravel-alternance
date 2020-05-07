6bfeabc391020472bc28a61dc1da4e02c9c3b361
Arthur Geay <arthurgeay.contact@gmail.com>
2020-05-07 15:14:55 +0200

    Add a api_token field

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'alcohol'
+        'name', 'email', 'password', 'alcohol', 'api_token'
     ];
 
     /**
