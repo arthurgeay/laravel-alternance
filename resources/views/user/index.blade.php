8538ba97c358cc3943c9343cf5f15b120d0e95d8
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-05-07 17:49:19 +0200

    styling users page

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -6,7 +6,7 @@
 
             <h1>Liste des utilisateurs</h1><br>
             @foreach($users as $user)
-            <div class="d-flex flex-row align-items-center shadow mb-4 p-4" style="padding-bottom:0!important;width:fit-content;border-radius:8px;">
+            <div class="d-flex flex-row align-items-center shadow mb-4 p-4" style="padding-bottom:0!important;border-radius:8px;">
                 <h3>{{ $user->name }}<span class="badge">
                 @if ($user->alcohol == 0)
                     @if ($user->applications_count < 10)
@@ -29,8 +29,11 @@
                         <img src="{{URL::asset('badges/corona.png')}}" alt="" style="position:relative;top:-.5rem;width:3.5rem" >
                     @endif
                 @endif
-                </span></h3>
-                <h3><alt="" style="position:relative;top:-.5rem;width:3.5rem"/>{{ $user->applications_count }} {{ $user->applications_count > 1 ? 'demandes' : 'demande'}}</h3>
+                </span>
+                </h3>
+                <p style="font-size:1.2rem; margin-left:auto;display:inline-block;margin-bottom: 1.5rem">
+                        <span style="font-weight:bold;">{{ $user->applications_count }}</span> {{ $user->applications_count > 1 ? 'demandes' : 'demande'}}
+                </p>
                 </div>
             @endforeach
 
