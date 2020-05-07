19b06cb11b1075d5afbfaa8819ba9a14d42a85bd
Kobweb <yugoplixgamer@gmail.com>
2020-05-07 14:42:26 +0200

    Add company.all api function

--- app/Http/Controllers/ApiCompanyController.php
+++ app/Http/Controllers/ApiCompanyController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Company;
 
 class ApiCompanyController extends Controller
 {
-    //
+    public function index(){
+        $companies = Company::all();
+        return response()->json([
+            'companies' => $companies
+        ]);
+    }
 }
