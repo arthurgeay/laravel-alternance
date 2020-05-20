c5f3f5ae8edc902188023661d7d80658f89a264e
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-05-20 13:35:18 +0200

    adding postgres

--- config/database.php
+++ config/database.php
@@ -14,7 +14,13 @@ return [
     | you may use many connections at once using the Database library.
     |
     */
+    url = parse_url(getenv("DATABASE_URL"));
 
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+    
     'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
@@ -52,8 +58,8 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+            'charset' => 'utf8',
+            'collation' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
@@ -66,11 +72,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', $host),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', $database),
+            'username' => env('DB_USERNAME', $username),
+            'password' => env('DB_PASSWORD', $password),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
