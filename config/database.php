dd66133a10f468f7191fa96b0cb3bef2db95b389
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-05-20 13:37:36 +0200

    env

--- config/database.php
+++ config/database.php
@@ -14,13 +14,7 @@ return [
     | you may use many connections at once using the Database library.
     |
     */
-    url = parse_url(getenv("DATABASE_URL"));
 
-    $host = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $database = substr($url["path"], 1);
-    
     'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
@@ -72,11 +66,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', $host),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', $database),
-            'username' => env('DB_USERNAME', $username),
-            'password' => env('DB_PASSWORD', $password),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
