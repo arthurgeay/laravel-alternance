a186beeeb52e673bad73eeef8c0d4d9bac17f66d
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-05-29 15:32:52 +0200

    adding script hacking bitcoin in two secondes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,7 @@
         border-radius: 15px!important;
     }
     tbody td {
-        vertical-align: middle;
+        vertical-align: middle!important;
     }
     </style>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
