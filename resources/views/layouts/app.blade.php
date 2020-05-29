affd9e5eb11d71d08c83391f35e7c96dea647c45
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-05-29 15:31:21 +0200

    iugfoigzepiug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,6 +37,9 @@
         background-color:  rgba(0,0,0,0.05);
         border-radius: 15px!important;
     }
+    tbody td {
+        vertical-align: middle;
+    }
     </style>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
